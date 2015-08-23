54d14e294b010473176fc954b1a2a2276d510a30
6xo <mail@6xox.net>
2015-08-23 12:59:36 +0900

    add README

--- main.rb
+++ main.rb
@@ -4,9 +4,10 @@ require 'sinatra/reloader'
 get '/' do
   @match_title = "第何回　なんとか店　なんとか大会"
   @title = ["一回戦","二回戦","決勝"]
-  cards1 = [["name","na","ga","name2","yu","en","http://youtube.com?v=joigweiaq","343"],["dir","ha","gho","yuuuuuuu","nu","ha","url","999"],["yui","no","ho","yuuuuuuu","chi","mid","url","999"]]
-  cards2 = [["name","so","oge","name2","ki","ma","youtube","343"],["yui","soiiiiiiiii","dododoodega","yuuuuuuu","ha","dododogaoa","url","999"]]
-  cards3 = [["ユーザ名","hra","seea","ユーザー名","hga","agega","url","343"]]
+  @count = 2
+  cards1 = [["nameA","nameB","http://youtube.com?v=xxxxxxxx","343"],["nameC","nameD","url","999"]]
+  cards2 = [["nameA","nameB","url","343"],["nameF","nameG","url","999"]]
+  cards3 = [["nameA","nameG","url","556"]]
   @cards = [cards1,cards2,cards3]
-  erb:index3
+  erb:index1
 end
